6b4ee3e2496e785bdc9c4a486f2af66fe79ed50d
Mathieu Bamert <pv23aha@eduvaud.ch>
2025-09-10 12:10:50 +0200

    Feat(mib-map)J'ai commencer à faire l'exercice

---
+++ Personnel/exos/mib-map/mibmab/mibmab/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using mibmab;
+
+
+List<Product> products = new List<Product>
+{
+    new Product { Location = 1, Producer = "Bornand", ProductName = "Pommes", Quantity = 20,Unit = "kg", PricePerUnit = 5.50 },
+    new Product { Location = 1, Producer = "Bornand", ProductName = "Poires", Quantity = 16,Unit = "kg", PricePerUnit = 5.50 },
+    new Product { Location = 1, Producer = "Bornand", ProductName = "Pastèques", Quantity = 14,Unit = "pièce", PricePerUnit = 5.50 },
+    new Product { Location = 1, Producer = "Bornand", ProductName = "Melons", Quantity = 5,Unit = "kg", PricePerUnit = 5.50 },
+    new Product { Location = 2, Producer = "Dumont", ProductName = "Noix", Quantity = 20,Unit = "sac", PricePerUnit = 5.50 },
+    new Product { Location = 2, Producer = "Dumont", ProductName = "Raisin", Quantity = 6,Unit = "kg", PricePerUnit = 5.50 },
+    new Product { Location = 2, Producer = "Dumont", ProductName = "Pruneaux", Quantity = 13,Unit = "kg", PricePerUnit = 5.50 },
+    new Product { Location = 2, Producer = "Dumont", ProductName = "Myrtilles", Quantity = 12,Unit = "kg", PricePerUnit = 5.50 },
+
+
+};
+
+Dictionary<string, string> i18n = new Dictionary<string, string>()
+{
+    { "Pommes","Apples"},
+    { "Poires","Pears"},
+    { "Pastèques","Watermelons"},
+    { "Melons","Melons"},
+    { "Noix","Nuts"},
+    { "Raisin","Grapes"},
+    { "Pruneaux","Plums"},
+    { "Myrtilles","Blueberries"},
+    { "Groseilles","Berries"},
+    { "Tomates","Tomatoes"},
+    { "Courges","Pumpkins"},
+    { "Pêches","Peaches"},
+    { "Haricots","Beans"}
+};
+
+
+
+
+Console.WriteLine(i18n["Poires"]);
