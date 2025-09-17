ebd99b75780372786faa873ce714485b0ea81971
MBAMER <163824331+MBAMER@users.noreply.github.com>
2025-09-17 11:57:05 +0200

    Feat(trackpoint.cs)Coder le fichier trackpoint.cs

---
+++ Personnel/exos/rando/RandoBase/Rando/Rando/Trackpoint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rando
+{
+    class Trackpoint
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Elevation { get; set; }
+
+        public Trackpoint(double latitude, double longitude, double elevation)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+            Elevation = elevation;
+        }
+    }
+}
+
